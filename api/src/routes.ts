b72e0a5e2a7e10a2288ab9d22f6f625ca7cb1f49
ricassiocosta <riksiocosta@gmail.com>
2020-10-13 13:10:31 -0300

    uploading images

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express"
+import multer from "multer"
 
 import OrphanagesController from "./controllers/OrphanagesController"
+import uploadConfig from "./config/upload"
 
 const routes = Router()
+const upload = multer(uploadConfig)
 
 routes
   .get("/orphanages", OrphanagesController.index)
   .get("/orphanages/:id", OrphanagesController.show)
-  .post("/orphanages", OrphanagesController.create)
+  .post("/orphanages", upload.array("images"), OrphanagesController.create)
 
 export default routes
