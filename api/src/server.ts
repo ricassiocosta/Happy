8551c690dc7025f3cefc01bc909052de968307b6
ricassiocosta <riksiocosta@gmail.com>
2020-10-13 11:19:55 -0300

    migration to create a orphanage created

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,39 @@
 import express from "express"
+import { getRepository } from "typeorm"
+import Orphanage from "./models/Orphanage"
+
 import "./database/connection"
 
 const app = express()
 
 app.use(express.json())
 
-app.get("/users", (req, res) => {
-  return res.json({ message: "Hello World" })
+app.post("/orphanages", async (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = req.body
+
+  const orphanagesRepository = getRepository(Orphanage)
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  })
+
+  await orphanagesRepository.save(orphanage)
+
+  return res.status(201).json(orphanage)
 })
 
 app.listen(3333)
