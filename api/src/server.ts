7eb28347a7166d09288951e9eee06e411298c4ea
ricassiocosta <riksiocosta@gmail.com>
2020-10-13 10:38:13 -0300

    connecting to sqlite

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express"
+import "./database/connection"
 
 const app = express()
 
-app.get("/", (req, res) => {
-  return res.json("Hello World")
+app.use(express.json())
+
+app.get("/users", (req, res) => {
+  return res.json({ message: "Hello World" })
 })
 
 app.listen(3333)
