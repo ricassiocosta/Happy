65a4a500172b8d4035dfdd8eed93bf40226c274d
ricassiocosta <riksiocosta@gmail.com>
2020-10-14 13:26:04 -0300

    setting google maps routes

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -86,7 +86,13 @@ export default function Orphanage() {
               </Map>
 
               <footer>
-                <a href="">Ver rotas no Google Maps</a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}
+                >
+                  Ver rotas no Google Maps
+                </a>
               </footer>
             </div>
 
